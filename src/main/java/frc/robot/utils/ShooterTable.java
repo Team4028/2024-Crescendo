0f50578db4a5aee565ed1a58db84124e1c3b2e0f
ETHendrix <ezekielhendrix@me.com>
2024-02-10 15:02:58 -0500

    ShooterTable Log Entry commit

--- src/main/java/frc/robot/utils/ShooterTable.java
+++ src/main/java/frc/robot/utils/ShooterTable.java
@@ -12,31 +12,40 @@ public class ShooterTable {
         public ShooterTableEntry(double angle, double leftSpeed, double distance, double rightSpeed) {
             this.angle = angle;
             this.leftSpeed = leftSpeed;
-            this.distance = distance;
             this.rightSpeed = rightSpeed;
+            this.distance = distance;
+
         }
 
     }
 
-    ArrayList<ShooterTableEntry> table = new ArrayList<>();
+    private static ArrayList<ShooterTableEntry> table = new ArrayList<>();
 
-    private void fillInTable() {
+    private static void fillInTable() {
         // put entries here
+        // Distances must go from top to bottom: shortest to longest
+        table.add(new ShooterTableEntry(0, 6, 0, 3));
+        table.add(new ShooterTableEntry(0, 2, 1, 0));
+        table.add(new ShooterTableEntry(0, 5, 4, 2));
+        table.add(new ShooterTableEntry(0, 7, 5, 3));
+        table.add(new ShooterTableEntry(0, 5, 3, 6));
     }
 
-    public ShooterTable() {
+    static {
         fillInTable();
     }
 
-    public ShooterTableEntry calcShooterTableEntry(double distance) {
+    public static ShooterTableEntry calcShooterTableEntry(double distance) {
         ShooterTableEntry closestLower = table.get(0);
         ShooterTableEntry closestHigher = table.get(table.size() - 1);
 
         // loop thru all of the entrys of the shootertable
         for (ShooterTableEntry entry : table) {
-            if (entry.distance < distance && (distance - closestLower.distance > distance - entry.distance)) {
+            if (entry.distance < distance
+                    && (Math.abs(distance - closestLower.distance) > Math.abs(distance - entry.distance))) {
                 closestLower = entry;
-            } else if (entry.distance > distance && (closestHigher.distance - distance > entry.distance - distance)) {
+            } else if (entry.distance > distance
+                    && (Math.abs(closestHigher.distance - distance) > Math.abs(entry.distance - distance))) {
                 closestHigher = entry;
             } else if (entry.distance == distance) {
                 closestHigher = entry;
@@ -47,12 +56,19 @@ public class ShooterTable {
 
         double scaleFactor = (distance - closestLower.distance) / (closestHigher.distance - closestLower.distance);
 
-        double calculatedLeftSpeed = scaleFactor * (closestHigher.leftSpeed - closestLower.leftSpeed);
-        double calculatedRightSpeed = scaleFactor * (closestHigher.rightSpeed - closestLower.rightSpeed);
+        double calculatedLeftSpeed = scaleFactor * (closestHigher.leftSpeed - closestLower.leftSpeed)
+                + closestLower.leftSpeed;
+        double calculatedRightSpeed = scaleFactor * (closestHigher.rightSpeed - closestLower.rightSpeed)
+                + closestLower.rightSpeed;
 
-        double calculatedAngle = scaleFactor * (closestHigher.angle - closestLower.angle);
+        double calculatedAngle = scaleFactor * (closestHigher.angle - closestLower.angle) + closestLower.angle;
+
+        System.out.println("Lower: " + closestLower.distance);
+        System.out.println("Higher: " + closestHigher.distance);
+        System.out.println(scaleFactor);
 
         return new ShooterTableEntry(calculatedAngle, calculatedLeftSpeed, calculatedRightSpeed, distance);
 
     }
+
 }
