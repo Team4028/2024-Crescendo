7d7a4db41c58e2497168b692f71f76088e7f3f0c
AndrewG3343 <ghabrial.andy@gmail.com>
2024-02-10 12:59:55 -0500

    Burn and Rainbow Animation

--- src/main/java/frc/robot/subsystems/Light.java
+++ src/main/java/frc/robot/subsystems/Light.java
@@ -6,20 +6,41 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.led.CANdle;
 import com.ctre.phoenix.led.CANdle.LEDStripType;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+import com.ctre.phoenix.led.Animation;
+import com.ctre.phoenix.led.ColorFlowAnimation;
+import com.ctre.phoenix.led.FireAnimation;
+import com.ctre.phoenix.led.RainbowAnimation;
+import com.ctre.phoenix.led.SingleFadeAnimation;
+import com.ctre.phoenix.led.StrobeAnimation;
+import com.ctre.phoenix.led.ColorFlowAnimation.Direction;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+
 
 public class Light extends SubsystemBase {
   private static Light instance;
-  private CANdle m_candle;
+  private Color m_color;
+  private CANdle candle;
+  private final int NUM_LEDS = 119;
+  private final int STRIP_LENGTH = 51;
 
   enum Color {
 
-    BLUE(0,0,245),
-    GREEN(0,245,0),
-    RED(245,0,0),
-    WALTER_WHITE(254, 254, 254),
-    HOT_PINK(254, 0, 170);
+    GREEN(0, 254, 0), 
+    PURPLE(118, 0, 254), 
+    ORANGE(254, 55, 0), 
+    BLUE(0, 0, 254), 
+    WHITE(254, 254, 254), 
+    RED(254, 0, 0), 
+    OFF(0, 0, 0);
 
     public int r;
     public int g;
@@ -34,20 +55,39 @@ public class Light extends SubsystemBase {
 
   /** Creates a new light. */
   private Light() {
-    m_candle = new CANdle(21, "rio");
-    m_candle.configBrightnessScalar(1.);
-    m_candle.configLEDType(LEDStripType.GRB);
-    setLEDsToColor(Color.WALTER_WHITE);
+    candle = new CANdle(21, "rio");
+    candle.configBrightnessScalar(1.);
+    candle.configLEDType(LEDStripType.GRB);
+    setColor(Color.WHITE);
   }
 
-  public void setLEDsToColor(Color color) {
-    m_candle.setLEDs(color.r, color.g, color.b);
+  public void setColor(Color color) {
+    m_color = color;
+  }
+
+  public void setNoColor(Color color){
+  setColor(Color.OFF); 
   }
 
   public static Light getInstance() {
     return instance == null ? instance = new Light() : instance;
   }
+  public FireAnimation BurnyBurn() {
+    return new FireAnimation(1, .5, NUM_LEDS, .5, .5);
+  }
+  
+  public RainbowAnimation Rainbow() {
+    return new RainbowAnimation();
+  }
 
+  public Command runBurnyBurnCommand() {
+    return runOnce (() -> BurnyBurn());
+  }
+
+  public Command runRainbowAnimationCommnad() {
+    return runOnce(() -> Rainbow());
+  }
+  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
