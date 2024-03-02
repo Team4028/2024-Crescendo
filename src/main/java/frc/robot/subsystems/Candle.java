2e060989e7936071692bb2abc91203b9dfecfd0c
sponge254 <117952984+gabeStuk@users.noreply.github.com>
2024-03-02 15:47:42 -0500

    Blink, fade, new colors! :sparkles:

--- src/main/java/frc/robot/subsystems/Candle.java
+++ src/main/java/frc/robot/subsystems/Candle.java
@@ -5,10 +5,13 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.led.CANdle;
-import com.ctre.phoenix.led.CANdle.LEDStripType;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.led.FireAnimation;
 import com.ctre.phoenix.led.RainbowAnimation;
+import com.ctre.phoenix.led.SingleFadeAnimation;
+import com.ctre.phoenix.led.StrobeAnimation;
+import com.ctre.phoenix.led.CANdle.LEDStripType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class Candle extends SubsystemBase {
@@ -18,12 +21,13 @@ public class Candle extends SubsystemBase {
 
     enum Color {
 
-        GREEN(0, 254, 0),
-        PURPLE(118, 0, 254),
-        ORANGE(254, 55, 0),
-        BLUE(0, 0, 254),
-        WHITE(254, 254, 254),
         RED(254, 0, 0),
+        ORANGE(254, 55, 0),
+        YELLOW(254, 254, 0),
+        GREEN(0, 254, 0),
+        BLUE(0, 0, 254),
+        PURPLE(118, 0, 254),
+        WHITE(254, 254, 254),
         OFF(0, 0, 0);
 
         public int r;
@@ -71,16 +75,32 @@ public class Candle extends SubsystemBase {
         return new RainbowAnimation();
     }
 
+    public StrobeAnimation blinkAnimation() {
+        return new StrobeAnimation(color.r, color.g, color.b);
+    }
+
+    public SingleFadeAnimation fadeAnimation() {
+        return new SingleFadeAnimation(color.r, color.g, color.b);
+    }
+
     public Command runBurnyBurnCommand() {
         return runOnce(() -> candle.animate(burnyBurn()));
     }
 
-    public Command runRainbowAnimationCommnad() {
+    public Command runRainbowAnimationCommand() {
         return runOnce(() -> candle.animate(rainbow()));
     }
 
-    //TODO: Add following options: Blinking, quick-flash, fade/breathe mode, more colors???
+    public Command runBlinkCommand() {
+        return runOnce(() -> candle.animate(blinkAnimation()));
+    }
 
+    public Command runFadeCommand() {
+        return runOnce(() -> candle.animate(fadeAnimation()));
+    }
+
+    // TODO: Add following options: Blinking, quick-flash, fade/breathe mode, more
+    // colors???
 
     @Override
     public void periodic() {
