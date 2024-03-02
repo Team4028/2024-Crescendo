6409e28860080ef2e08e2ad6016104fe9e6770ab
Gabe <gabe.seq@gmail.com>
2024-03-02 13:45:39 -0500

    candle

---
+++ src/main/java/frc/robot/subsystems/Candle.java
@@ -0,0 +1,89 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.led.CANdle;
+import com.ctre.phoenix.led.CANdle.LEDStripType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.led.FireAnimation;
+import com.ctre.phoenix.led.RainbowAnimation;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class Candle extends SubsystemBase {
+    private final CANdle candle;
+    private final int NUM_LEDS = 69;
+    private Color color;
+
+    enum Color {
+
+        GREEN(0, 254, 0),
+        PURPLE(118, 0, 254),
+        ORANGE(254, 55, 0),
+        BLUE(0, 0, 254),
+        WHITE(254, 254, 254),
+        RED(254, 0, 0),
+        OFF(0, 0, 0);
+
+        public int r;
+        public int g;
+        public int b;
+
+        Color(int r, int g, int b) {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+    }
+
+    /** Creates a new light. */
+    private Candle() {
+        candle = new CANdle(21, "rio");
+        candle.configBrightnessScalar(.5);
+        candle.configLEDType(LEDStripType.GRB);
+        setColor(Color.WHITE);
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+        candle.animate(null);
+        candle.setLEDs(this.color.r, this.color.g, this.color.b);
+    }
+
+    public void setNoColor() {
+        setColor(Color.OFF);
+    }
+
+    public Command setColorGreenCommand() {
+        return runOnce(() -> setColor(Color.GREEN));
+    }
+
+    public Command setNoColorCommand() {
+        return runOnce(() -> setColor(Color.OFF));
+    }
+
+    public FireAnimation burnyBurn() {
+        return new FireAnimation(1, .5, NUM_LEDS, .5, .5);
+    }
+
+    public RainbowAnimation rainbow() {
+        return new RainbowAnimation();
+    }
+
+    public Command runBurnyBurnCommand() {
+        return runOnce(() -> candle.animate(burnyBurn()));
+    }
+
+    public Command runRainbowAnimationCommnad() {
+        return runOnce(() -> candle.animate(rainbow()));
+    }
+
+    //TODO: Add following options: Blinking, quick-flash, fade/breathe mode, more colors???
+
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+}
