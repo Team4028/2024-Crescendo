1b55ae5d45fc5403317a08cdf9cf8c9960ae5ede
Gabe <gabe.seq@gmail.com>
2024-10-26 11:22:40 -0400

    update global shooter offset

--- src/main/java/frc/robot/utils/ShooterTable.java
+++ src/main/java/frc/robot/utils/ShooterTable.java
@@ -85,7 +85,7 @@ public class ShooterTable {
 
     private static void fillInTable() {
 
-        final double GLOBAL_OFFSET = 0.375; // offset from pre-iri good
+        final double GLOBAL_OFFSET = 0.5; // all shooter table entries use this value
         // put entries here
         // Distances must go from top to bottom: shortest to longest
         // IF YOU INCREASE DISTANCE, SHOOTER ANGLE GOES UP
